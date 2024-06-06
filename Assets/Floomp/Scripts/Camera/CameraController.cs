65b2845903d608528715ddf4d00770d96c0cb6b8
AndyCreighton87 <andrewcreighton87@gmail.com>
2024-06-06 21:08:12 +0100

    Camera pan

--- Assets/Floomp/Scripts/Camera/CameraController.cs
+++ Assets/Floomp/Scripts/Camera/CameraController.cs
@@ -17,12 +17,15 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float minY = 20.0f;
     [SerializeField] private float maxY = 120.0f;
 
+    [SerializeField] Transform groundTransform;
+
     private Vector3 panDirection;
     private float scrollAmount;
 
-    // Start is called before the first frame update
     void Start()
     {
+        SetupGroundTransform();
+
         InputHandler inputHandler = InputHandler.Instance;
 
         // Horizontal Input
@@ -36,7 +39,17 @@ public class CameraController : MonoBehaviour
         //Zoom
         inputHandler.OnZoom.AddListener(Zoom);
         inputHandler.OnZoomEnded.AddListener(ZoomEnded);
+    }
 
+    private void SetupGroundTransform() {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Debug.DrawRay(ray.origin, ray.direction * 1000f, Color.green);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorLayer)) {
+            groundTransform.position = hit.point;
+        }
+        else {
+            groundTransform.position = Vector3.zero;
+        }
     }
 
     private void PanHorizontal(float _value) {
@@ -65,47 +78,27 @@ public class CameraController : MonoBehaviour
     }
 
     private void Update() {
+        if (CheckCanMove()) {
+            Vector3 newPos = transform.position + panDirection;
 
-        //Vector3 newPos = transform.position + panDirection;
-        //newPos.y -= scrollAmount;
-        //
-        //Vector3 clampedPos = GetClampedPosition(newPos);
-        //
-        //transform.position = clampedPos;
+            newPos.y -= scrollAmount;
+            newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
 
-
-        Vector3 newPos = transform.position + panDirection;
-        
-        newPos.x = Mathf.Clamp(newPos.x, -panLimitX, panLimitX);
-        newPos.z = Mathf.Clamp(newPos.z, -panLimitZ, panLimitZ);
-        
-        newPos.y -= scrollAmount;
-        newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
-        
-        transform.position = newPos;
+            transform.position = newPos;
+        }
     }
 
-    //private Vector3 GetClampedPosition(Vector3 _targetPosition) {
-    //    // Perform a raycast from the center of the camera's viewport
-    //    Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-    //    Debug.DrawRay(ray.origin, ray.direction * 1000f, Color.green);
-    //    if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorLayer)) {
-    //        Vector3 floorPoint = hit.point;
-    //        Debug.Log($"Floor point: {floorPoint}");
-    //
-    //        // Adjust the floor point based on the target position
-    //        floorPoint.x = Mathf.Clamp(_targetPosition.x, -panLimitX, panLimitX);
-    //        floorPoint.z = Mathf.Clamp(_targetPosition.z, -panLimitZ, panLimitZ);
-    //
-    //        // Adjust the target position's height
-    //        _targetPosition.y = Mathf.Clamp(_targetPosition.y, minY, maxY);
-    //
-    //        Debug.Log($"Clamped Pos: {new Vector3(floorPoint.x, _targetPosition.y, floorPoint.z)}");
-    //
-    //        return new Vector3(floorPoint.x, _targetPosition.y, floorPoint.z);
-    //    }
-    //
-    //    // Return the original target position if the raycast fails
-    //    return _targetPosition;
-    //}
+    private bool CheckCanMove() {
+        Vector3 newPos = groundTransform.position + panDirection;
+
+        if (newPos.x <= -panLimitX || newPos.x >= panLimitX) {
+            return false;
+        }
+
+        if (newPos.z <= -panLimitZ || newPos.z >= panLimitZ) {
+            return false;
+        }
+
+        return true;
+    }
 }
